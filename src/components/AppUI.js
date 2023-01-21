e6a2f640253651b320d0ca0d70d894f18a1872fb
Devnielote <danielfc_26@hotmail.com>
2023-01-20 20:08:58 -0600

    Create contact component and its styles

--- src/components/AppUI.js
+++ src/components/AppUI.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { TopNav } from "./TopNav";
 import { Header } from "./Header";
+import { Contact } from "./Contact";
 import ".././css/TopNav.css";
 import ".././css/Header.css";
+import "../css/Contact.css";
 
 function AppUI() {
   return (
     <>
       <TopNav />
       <Header />
+      <Contact />
     </>
   );
 }
